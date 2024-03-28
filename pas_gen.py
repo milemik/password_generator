a0c0929c115df19fdb3afb677005bf1eb7ca6eb7
milemik <milemik68@gmail.com>
2024-03-28 17:56:07 +0100

    added some comments

--- pas_gen.py
+++ pas_gen.py
@@ -4,7 +4,7 @@ import json
 from datetime import datetime
 
 
-class Password_gen():
+class Password_gen:
 
     def __init__(self):
         # objects
@@ -14,10 +14,14 @@ class Password_gen():
         self.numbers = [x for x in range(0, 30)]
         self.strong_check = 0
         td = datetime.now()
-        self.when_time = f"{td.day} {td.month} {td.year}T{td.hour}-{td.minute}-{td.second}"
+        self.when_time = (
+            f"{td.day} {td.month} {td.year}T{td.hour}-{td.minute}-{td.second}"
+        )
         while True:
             try:
-                self.strong = int(input("How strong would you like your pass to be? (0-30)\n"))
+                self.strong = int(
+                    input("How strong would you like your pass to be? (0-30)\n")
+                )
                 if 0 < self.strong < 31:
                     print("Input ok")
                     break
@@ -27,19 +31,18 @@ class Password_gen():
                 print("Plase enter valid number")
         self.password = ""
 
-
     # GET RANDOM DATA
     def random(self):
         while True:
             add = random.choice([self.random_ll, self.random_ul, self.random_num])
-            #print(add())
+            # print(add())
             self.password += add()
             self.strong_check += 1
             if self.strong_check == self.strong:
                 break
         return self.password
 
-    #random lower letter
+    # random lower letter
     def random_ll(self):
         return random.choice(self.lower_letters)
 
@@ -49,10 +52,12 @@ class Password_gen():
 
     # radnom number
     def random_num(self):
+        """Will return random number as string"""
         return str(random.choice(self.numbers))
 
     # Write to json file
     def write_to_file(self):
+        """Will write generate JSON file with password info"""
         data = {"name": self.name, "time": self.when_time, "password": self.password}
         with open(f"PASSWORD {self.when_time}.json", "w") as f:
             f.write(json.dumps(data, indent=2))
@@ -61,8 +66,8 @@ class Password_gen():
         return f"Your pass is: {self.password}"
 
 
-
 def main():
+    """Main function to execute generating password"""
     s = Password_gen()
     s.random()
     s.write_to_file()
