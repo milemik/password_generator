999bebf68ad93f64d00805adabcf6ab69313fdd7
milemik <milemik68@gmail.com>
2024-03-28 17:39:13 +0100

    refactor

--- pas_gen.py
+++ pas_gen.py
@@ -1,72 +1,87 @@
 import random
 import string
 import json
+import logging
+
 from datetime import datetime
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename="pwdgen.log", encoding="utf-8", level=logging.DEBUG)
 
-class Password_gen():
 
-    def __init__(self):
+def mlog(msg: str) -> None:
+    logger.info(msg=msg)
+
+
+def time_to_str() -> str:
+    td = datetime.now()
+    return f"{td.day} {td.month} {td.year}T{td.hour}-{td.minute}-{td.second}"
+
+
+def ask_imput() -> int:
+    while True:
+        try:
+            strong = int(input("How strong would you like your pass to be? (0-30)\n"))
+            if 0 < strong < 31:
+                mlog(msg="Input ok")
+                return strong
+            else:
+                print("Please enter number betwen 0 and 10")
+        except ValueError:
+            mlog("Plase enter valid number")
+
+
+class Password_gen:
+    LOVER_LETTERS = string.ascii_lowercase
+    UPPER_LETTERS = string.ascii_uppercase
+    NUMBERS = [x for x in range(0, 30)]
+
+    def __init__(self, strength: int):
         # objects
-        self.name = input("Name the password:\n")
-        self.lower_letters = string.ascii_lowercase
-        self.upper_letters = string.ascii_uppercase
-        self.numbers = [x for x in range(0, 30)]
-        self.strong_check = 0
-        td = datetime.now()
-        self.when_time = f"{td.day} {td.month} {td.year}T{td.hour}-{td.minute}-{td.second}"
-        while True:
-            try:
-                self.strong = int(input("How strong would you like your pass to be? (0-30)\n"))
-                if 0 < self.strong < 31:
-                    print("Input ok")
-                    break
-                else:
-                    print("Please enter number betwen 0 and 10")
-            except ValueError:
-                print("Plase enter valid number")
-        self.password = ""
-
+        self.name: str = input("Name the password:\n")
+        self.when_time: str = time_to_str()
+        self.strong: int = strength
 
     # GET RANDOM DATA
-    def random(self):
+    def random(self) -> str:
+        password = ""
+        strong_check: int = 0
         while True:
             add = random.choice([self.random_ll, self.random_ul, self.random_num])
-            #print(add())
-            self.password += add()
-            self.strong_check += 1
-            if self.strong_check == self.strong:
+            password += add()
+            strong_check += 1
+            if strong_check == self.strong:
                 break
-        return self.password
+        mlog(msg=f"GENERATED PASS: {password}")
+        return password
 
-    #random lower letter
+    # random lower letter
     def random_ll(self):
-        return random.choice(self.lower_letters)
+        return random.choice(self.LOVER_LETTERS)
 
     # random UPPER LETTER
     def random_ul(self):
-        return random.choice(self.upper_letters)
+        return random.choice(self.UPPER_LETTERS)
 
     # radnom number
     def random_num(self):
-        return str(random.choice(self.numbers))
+        return str(random.choice(self.NUMBERS))
 
     # Write to json file
-    def write_to_file(self):
-        data = {"name": self.name, "time": self.when_time, "password": self.password}
+    def write_to_file(self, password: str) -> None:
+        data = {"name": self.name, "time": self.when_time, "password": password}
         with open(f"PASSWORD {self.when_time}.json", "w") as f:
             f.write(json.dumps(data, indent=2))
 
-    def __str__(self):
-        return f"Your pass is: {self.password}"
-
+    def run(self):
+        password: str = self.random()
+        self.write_to_file(password=password)
 
 
 def main():
-    s = Password_gen()
-    s.random()
-    s.write_to_file()
-    print(s)
+    user_input: int = ask_imput()
+    s = Password_gen(strength=user_input)
+    s.run()
 
 
 if __name__ == "__main__":
